736b0b94f96515187c2bd6c9c5ed9e58c9800974
Darryl Bond <darryl.bond@gmail.com>
2022-12-29 15:20:49 +1000

    Added some python module documentation

--- usbrelay_py/src/__init__.py
+++ usbrelay_py/src/__init__.py
@@ -4,5 +4,20 @@ usbrelay_py
 Python bindings to the usbrelay command line tool
 """
 
+"""
+Control usb connected relays with ID 16c0:05df (Van Ooijen Technische Informatica HID device except mice, keyboards, and joysticks)
+
+Module Init
+import usbrelay_py
+
+Module Methods:
+usbrelay_board_count() returns the number of usbrelay boards found
+
+usbrelay_board_details() returns a set of Python tuples  (SERIAL of Relay Module,Number of RELAYS,Integer State of RELAYS)
+
+usbrelay_board_control('SERIAL',Relay number, Desired State)
+eg usbrelay_board_control('ABCDE',2,0)
+"""
+
 __version__ = "1.0"
 __author__ = "Sean Mollet"
